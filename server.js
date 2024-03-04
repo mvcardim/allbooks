4ca0b8e1eef4b8aee603abefddaf1629730b9d42
MARCO V M CARDIM <68335567+mvcardim@users.noreply.github.com>
2024-03-04 11:05:09 -0300

    Update server.js

--- server.js
+++ server.js
@@ -13,9 +13,7 @@ server.use(bodyParser.json())
 server.use(jsonServer.defaults());
 
 const SECRET_KEY = '123456789'
-console.log("Boas-vindas ao AllBooks")
-console.log("Essa é uma API de uma loja de livros online")
-console.log("Acesse essa API em http://localhost:8000")
+
 
 
 function createToken(payload, expiresIn = '12h') {
